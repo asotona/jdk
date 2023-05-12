d617c46f251918384f130ff22652d2d512ba4e94
Adam Sotona <asotona@openjdk.org>
2023-05-12 14:20:40 +0200

    SwitchBootstrap::generateInnerClass implementation

--- src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
+++ src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
@@ -25,6 +25,10 @@
 
 package java.lang.runtime;
 
+import java.lang.constant.ClassDesc;
+import java.lang.constant.ConstantDescs;
+import static java.lang.constant.ConstantDescs.*;
+import java.lang.constant.MethodTypeDesc;
 import java.lang.invoke.CallSite;
 import java.lang.invoke.ConstantBootstraps;
 import java.lang.invoke.ConstantCallSite;
@@ -33,9 +37,19 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.stream.Stream;
 
+import jdk.internal.classfile.Classfile;
 import jdk.internal.javac.PreviewFeature;
 
+import java.lang.invoke.LambdaConversionException;
+import static java.lang.invoke.MethodHandles.Lookup.ClassOption.NESTMATE;
+import static java.lang.invoke.MethodHandles.Lookup.ClassOption.STRONG;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
 import static java.util.Objects.requireNonNull;
+import static jdk.internal.classfile.Classfile.*;
+import jdk.internal.classfile.instruction.SwitchCase;
 
 /**
  * Bootstrap methods for linking {@code invokedynamic} call sites that implement
@@ -55,10 +69,16 @@ public class SwitchBootstraps {
     private static final MethodHandle DO_TYPE_SWITCH;
     private static final MethodHandle DO_ENUM_SWITCH;
 
+    private static final MethodType DO_TYPE_SWITCH_TYPE = MethodType.methodType(
+                                           int.class, Object.class, int.class, Object[].class);
+    private static final MethodType GENERATED_SWITCH_TYPE = MethodType.methodType(
+                                           int.class, Object.class, int.class);
+    private static final MethodTypeDesc MTD_GENERATED_SWITCH = GENERATED_SWITCH_TYPE.describeConstable().get();
+
     static {
         try {
             DO_TYPE_SWITCH = LOOKUP.findStatic(SwitchBootstraps.class, "doTypeSwitch",
-                                           MethodType.methodType(int.class, Object.class, int.class, Object[].class));
+                                           DO_TYPE_SWITCH_TYPE);
             DO_ENUM_SWITCH = LOOKUP.findStatic(SwitchBootstraps.class, "doEnumSwitch",
                                            MethodType.methodType(int.class, Enum.class, int.class, Object[].class));
         }
@@ -127,8 +147,125 @@ public class SwitchBootstraps {
         labels = labels.clone();
         Stream.of(labels).forEach(SwitchBootstraps::verifyLabel);
 
-        MethodHandle target = MethodHandles.insertArguments(DO_TYPE_SWITCH, 2, (Object) labels);
+        boolean hasOnlyClassLabels = Stream.of(labels).allMatch(l -> l instanceof Class<?>);
+        MethodHandle target;
+
+        if (hasOnlyClassLabels) {
+            try {
+                target = lookup.findStatic(generateInnerClass(lookup, labels), "typeSwitch", MethodType.methodType(int.class, Object.class, int.class));
+            } catch (LambdaConversionException | NoSuchMethodException | IllegalAccessException ex) {
+                throw new IllegalStateException(ex);
+            }
+        } else {
+            target = MethodHandles.insertArguments(DO_TYPE_SWITCH, 2, (Object) labels);
+        }
+
         return new ConstantCallSite(target);
+    }
+
+   @SuppressWarnings("removal")
+    private static Class<?> generateInnerClass(MethodHandles.Lookup caller, Object[] labels) throws LambdaConversionException {
+        Map<Integer, Class<?>> hashMap = new HashMap<>();
+        Map<Class<?>, BitSet> possitionsMap = new HashMap<>();
+        for (int i = 0; i < labels.length; i++) {
+            var cls = (Class<?>)labels[i];
+            var sub = cls.getPermittedSubclasses();
+            if (sub != null || (cls.getModifiers() & ACC_FINAL) != 0) {
+                hashMap.put(cls.hashCode(), cls);
+                possitionsMap.computeIfAbsent(cls, c -> new BitSet()).set(i);
+            }
+            if (sub != null) for (var sc : sub) {
+                hashMap.put(sc.hashCode(), sc);
+                possitionsMap.computeIfAbsent(sc, c -> new BitSet()).set(i);
+            }
+        }
+        //lookupSwitch (arg0.getClass().hash()) {
+        //    case #1 -> if (arg0 instanceof labelWith#1) {
+        //                  if (arg1 <= labelBitset#1) return #1
+        //                  else if (arg1 <= labelBitset#2) return #2
+        //                  else return labels.length;
+        //              } else return fallback();
+        //    default -> return fallback();
+        //
+        //fallback
+        //tableSwitch (arg1) {
+        //    case 0 -> if (arg0 instanceof label0) return 0; //fallthrough else
+        //    case 1 -> //fallthrough for labels handled in lookupswith
+        //    case 2 -> if (arg0 instanceof label2) return 2; //fallthrough else
+        //    default -> return labels.length;
+        final byte[] classBytes = Classfile.build(ClassDesc.of(caller.lookupClass().getPackageName(), "TypeSwitch"), clb -> clb
+                .withFlags(ACC_SUPER | ACC_FINAL | ACC_SYNTHETIC)
+                .withMethodBody("typeSwitch", MTD_GENERATED_SWITCH, ACC_PUBLIC | ACC_STATIC | ACC_FINAL, cob -> {
+                    var cont = cob.newLabel();
+                    cob.aload(0)
+                       .if_nonnull(cont)
+                       .iconst_m1()
+                       .ireturn()
+                       .labelBinding(cont);
+                    var end = cob.newLabel();
+                    if (labels.length > 0) {
+                        var fallback = cob.newLabel();
+                        var hashCases = new ArrayList<SwitchCase>(hashMap.size());
+                        for (int hash : hashMap.keySet()) {
+                            hashCases.add(SwitchCase.of(hash, cob.newLabel()));
+                        }
+
+                        //hash lookup
+                        cob.aload(0)
+                           .invokevirtual(CD_Object, "getClass", MethodTypeDesc.of(CD_Class))
+                           .invokevirtual(CD_Class, "hashCode", MethodTypeDesc.of(CD_int))
+                           .lookupswitch(fallback, hashCases);
+                        for (var c : hashCases) {
+                            var cls = hashMap.get(c.caseValue());
+                            cob.labelBinding(c.target())
+                               .aload(0)
+                               .instanceof_(cls.describeConstable().get())
+                               .ifeq(fallback);
+                            var possitions = possitionsMap.get(cls);
+                            for (int i = possitions.nextSetBit(0); i >= 0; i = possitions.nextSetBit(i+1)) {
+                                var skip = cob.newLabel();
+                                cob.iload(1)
+                                   .constantInstruction(i)
+                                   .if_icmpgt(i == possitions.length() - 1 ? end : skip)
+                                   .constantInstruction(i)
+                                   .ireturn()
+                                   .labelBinding(skip);
+                            }
+                        }
+
+                        //fallback
+                        var fallbackCases = new ArrayList<SwitchCase>(labels.length);
+                        for (int i = 0; i < labels.length; i++) {
+                            fallbackCases.add(SwitchCase.of(i, cob.newLabel()));
+                        }
+                        cob.labelBinding(fallback)
+                           .iload(1)
+                           .tableswitch(0, labels.length - 1, end, fallbackCases);
+                        for (var c : fallbackCases) {
+                            var fallThrough = cob.newLabel();
+                            cob.labelBinding(c.target())
+                               .aload(0)
+                               .instanceof_(((Class<?>)labels[c.caseValue()]).describeConstable().get())
+                               .ifeq(fallThrough)
+                               .constantInstruction(c.caseValue())
+                               .ireturn()
+                               .labelBinding(fallThrough);
+                        }
+                    }
+                    cob.labelBinding(end)
+                       .constantInstruction(labels.length)
+                       .ireturn();
+                }));
+        try {
+            // this class is linked at the indy callsite; so define a hidden nestmate
+            MethodHandles.Lookup lookup;
+            lookup = caller.defineHiddenClass(classBytes, true, NESTMATE, STRONG);
+            return lookup.lookupClass();
+        } catch (IllegalAccessException e) {
+            throw new LambdaConversionException("Exception defining lambda proxy class", e);
+        } catch (Throwable t) {
+            throw new InternalError(t);
+        }
     }
 
     private static void verifyLabel(Object label) {
